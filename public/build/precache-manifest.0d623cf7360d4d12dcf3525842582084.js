775e14bde8b7d077f924277666467d4b7ab2781b
Vlp90 <vladimir.leloup@gmail.com>
2020-09-23 17:16:57 +0200

    ready to deploy

---
+++ public/build/precache-manifest.0d623cf7360d4d12dcf3525842582084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18d82602226f2b5779e8e00b9f9a5e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1efb6344767847843da",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "b81d5a53828484e8b795",
+    "url": "/static/css/main.26af72aa.chunk.css"
+  },
+  {
+    "revision": "a1efb6344767847843da",
+    "url": "/static/js/2.dc915208.chunk.js"
+  },
+  {
+    "revision": "f6eaced1c9c2b08ff20fdaf0bd2c9285",
+    "url": "/static/js/2.dc915208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81d5a53828484e8b795",
+    "url": "/static/js/main.6f927d1e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
